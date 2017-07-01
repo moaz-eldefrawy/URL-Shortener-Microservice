cc51756d27c512bd7716e6668f2fe926a7ecaaa9
System <none>
2017-07-01 22:25:10 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -15,12 +15,24 @@ function validateUrl(value) {
   return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
 }
 
-// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html')
+});
+
+// handling requests
 app.get("*", function (req, res) {
+  
+  var response = {};
   res.send(req.url);
   var url = req.url.substring(0, req.url.length);
   if(validateUrl(url) == false) 
-    res.end()
+    res.error = "This url is not on the database.";
+  
+  else{
+    
+  }
+  
+  res.end(JSON.stringify(response));
 });
 
 
